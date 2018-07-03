9a6074c489fa9ead825719396465ded1632b6a7a
Marissa Wood <marissa.espina@gmail.com>
2018-07-03 08:58:53 -0400

    added delete functionality

--- index.js
+++ index.js
@@ -42,6 +42,27 @@ app.get('/api/items/:id', (req, res) => {
     })
 })
 
+app.delete('/api/items/:id', (req, res) => {
+  Item.findByIdAndRemove(req.params.id)
+    .then(item => {
+      if (!item) {
+        return res.status(404).send({
+          message: 'Item not found with id ' + req.params.id
+        })
+      }
+      res.send({message: 'Item deleted successfully!'})
+    }).catch(err => {
+      if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+        return res.status(404).send({
+          message: 'Note not found with id ' + req.params.id
+        })
+      }
+      return res.status(500).send({
+        message: 'Could not delete note with id ' + req.params.id
+      })
+    })
+})
+
 app.listen(app.get('port'), () => {
   console.log('Server listening on port ' + app.get('port'))
 })
